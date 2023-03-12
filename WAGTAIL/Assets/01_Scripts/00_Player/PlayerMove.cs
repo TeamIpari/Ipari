60acc5497b1b1c0b09f8ad247a003947b75e99e3
KangMyeongH <3044965@naver.com>
2023-03-12 20:53:53 +0900

    First

---
+++ WAGTAIL/Assets/01_Scripts/00_Player/PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    [SerializeField] public float Speed;
+
+    private float vAxis;
+    private float hAxis;
+
+    Vector3 moveVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * Speed * Time.deltaTime;
+
+        transform.LookAt(transform.position + moveVec);
+    }
+}
---
+++ WAGTAIL/Assets/01_Scripts/02_Object/InteractionSystem/Door.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string _promt;
+
+    public string InteractionPrompt => _promt;
+
+    public bool Interact(Interactor interactor)
+    {
+        // 예외 처리 하기
+
+        Debug.Log("Open Door!");
+        return true;
+    }
+}
