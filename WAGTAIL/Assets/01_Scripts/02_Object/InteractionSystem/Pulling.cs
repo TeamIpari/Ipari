78ecca4c933444b6b545c0e6eb900a7f9829431c
KangMyeongH <3044965@naver.com>
2023-03-30 23:43:17 +0900

    Setting
    초기 Scene세팅

--- WAGTAIL/Assets/01_Scripts/02_Object/InteractionSystem/Pulling.cs
+++ WAGTAIL/Assets/01_Scripts/02_Object/InteractionSystem/Pulling.cs
@@ -5,18 +5,10 @@ using UnityEngine;
 public class Pulling : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _promt;
-
-    [SerializeField] GameObject _playerEquipPoint;
-
     public string InteractionPrompt => _promt;
 
     public bool Interact(Interactor interactor)
     {
-        if(_playerEquipPoint.transform.childCount == 0 &&
-            interactor.player.movementSM.currentState == interactor.player.idle)
-        {
-            return true;   
-        }
 
 
         return true;
