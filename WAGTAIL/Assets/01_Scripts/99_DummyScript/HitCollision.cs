c1f9933fb75bd770a7ccb24b4f819d91cbde58c7
hoper7748 <hoper7748@gmail.com>
2023-04-05 03:31:54 +0900

    OpenDoor_Script
    HitCollision Script 추가 및 문을 여는 기능 추가

--- WAGTAIL/Assets/01_Scripts/99_DummyScript/HitCollision.cs
+++ WAGTAIL/Assets/01_Scripts/99_DummyScript/HitCollision.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class HitCollision : MonoBehaviour
 {
-    public GameObject Door_1;
-    public GameObject Door_2;
+    public Transform Door_1;
+    public Transform Door_2;
+    public float _Distance = 10.00f;
 
+    [Header("문이 열리는 속도")]
     public float _openSpeed = 1.0f;
+    [Header("문 열린 후 작동하는 시간.")]
+    public float _DelayTime = 1.0f;
 
     [SerializeField]
     private bool b_Open = false;
@@ -19,20 +23,45 @@ public class HitCollision : MonoBehaviour
 
     private void Update()
     {
-        
+        //_Distance += Time.deltaTime;
+        Debug.Log(_Distance);
+        if(b_Open && Vector3.Distance(Door_1.position, Door_2.position) <= _Distance)
+        {
+            Debug.Log("aa");
+            Door_1.Translate(Vector3.left * Time.deltaTime * _openSpeed);
+            Door_2.Translate(Vector3.right * Time.deltaTime * _openSpeed);
+        }
     }
 
     private void OpenDoor()
     {
-        Door_1.SetActive(false);
+        //Door_1.SetActive(false);
+        //b_Open = true;
+        StartCoroutine(DummyCo());
     }
+
+    IEnumerator DummyCo()
+    {
+        yield return null;
+        Door_1.position = new Vector3(Door_1.position.x - .15f, Door_1.position.y, Door_1.position.z);
+        Door_2.position = new Vector3(Door_2.position.x + .15f, Door_2.position.y, Door_2.position.z);
+
+        yield return new WaitForSeconds(_DelayTime);
+        Debug.Log(_Distance);
+        b_Open = true;
+
+        yield return null;
+
+
+    }
+
+
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == 6)
         {
             OpenDoor();
-            b_Open = true;
         }
     }
 }
