55e786add469fba0d2cbe4a3aff1988165893b0a
KangMyeongH <71129440+KangMyeongH@users.noreply.github.com>
2023-04-10 19:53:22 +0900

    PlayerController
    1. 움직이는 플랫폼 제작중

---
+++ WAGTAIL/Assets/01_Scripts/02_Object/PlatformManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+
+    // ¿òÁ÷ÀÌ´Â ÇÃ·§Æû 
+    [SerializeField] private Transform _startPoint;
+    [SerializeField] private Transform _endPoint;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _time;
+    [SerializeField] private Player _player;
+
+    private Vector3 _pointSize = new Vector3(1, 1, 1);
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(_startPoint.position, _pointSize);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(_endPoint.position, _pointSize);
+    }
+}
