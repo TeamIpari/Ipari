02d57cba9ef856303365da532497a03d4e49939c
KangMyeongH <71129440+KangMyeongH@users.noreply.github.com>
2023-04-16 22:39:40 +0900

    CameraSetup
    1. 카메라 수정 중
    2. 던지기 수정 중

---
+++ WAGTAIL/Assets/01_Scripts/02_Object/InteractionSystem/InteractionUIManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractionUIManager : MonoBehaviour
+{
+    [SerializeField] private Transform _interactionPoint;
+    [SerializeField] private float _interactionPointRadius = 1.0f;
+    [SerializeField] private LayerMask _interactableMask;
+    [SerializeField] private int _numFound;
+
+    private readonly Collider[] _colliders = new Collider[1];
+    private bool _isActive;
+    private Animator _animator;
+
+    //private static int testAniHash = Animator.StringToHash("fadein");
+
+
+    private void Start()
+    {
+        _isActive = false;
+        _animator = GetComponentInChildren<Animator>();
+        _animator.speed = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        interactionUI();
+    }
+
+    private void interactionUI()
+    {
+        _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, 
+            _colliders,_interactableMask);
+
+        if(_numFound > 0 && !_isActive)
+        {
+            _isActive = true;
+            if (_animator.speed > 0f)
+            {
+                _animator.SetTrigger("fadein");
+            }
+            _animator.speed = 1.0f;
+        }
+
+        else if(_numFound == 0 && _isActive)
+        {
+            _isActive = false;
+            _animator.SetTrigger("fadeout");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
+    }
+}
---
+++ WAGTAIL/Assets/01_Scripts/99_DummyScript/TrajectoryLine/Projection.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Projection : MonoBehaviour
+{
+    private Scene _simulationScene;
+    private PhysicsScene _physicsScene;
+    [SerializeField] private Transform _obstaclesParaent;
+
+    void CreatePhysicsScene()
+    {
+        _simulationScene = SceneManager.CreateScene("Simulation", new CreateSceneParameters(LocalPhysicsMode.Physics3D));
+        _physicsScene  = _simulationScene.GetPhysicsScene();
+
+        foreach(Transform obj in _obstaclesParaent)
+        {
+            var ghostObj = Instantiate(obj.gameObject, obj.position, obj.rotation);
+            ghostObj.GetComponent<Renderer>().enabled = false;
+            SceneManager.MoveGameObjectToScene(ghostObj, _simulationScene);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
