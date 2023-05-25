71f74ee4eee0190b340476a068f027f33853566a
JHSeong <50869470+hoper7748@users.noreply.github.com>
2023-05-26 00:31:19 +0900

    Boss Pattern
    보스 3번째 패턴 초안 제작

--- WAGTAIL/Assets/01_Scripts/01_NPC/BossScript/AI Agent/State/BossPattern/DummyAttack1.cs
+++ WAGTAIL/Assets/01_Scripts/01_NPC/BossScript/AI Agent/State/BossPattern/DummyAttack1.cs
@@ -1,26 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity;
 using UnityEngine;
 
 public class DummyAttack1 : AIState
 {
     float curTimer = 0;
-    float changeTimer = 3;
+    float changeTimer = 5f;
+
+    float curShowTimer = 0;
+    float showTimer = 0.5f;
 
     bool on = false;
+    GameObject tentacle;
     GameObject dangerousEffect;
 
-    public DummyAttack1(AIStateMachine _stateMachine, GameObject _danger) : base(_stateMachine)
+    public DummyAttack1(AIStateMachine _stateMachine, GameObject _tentacle, GameObject _danger) : base(_stateMachine)
     {
         //curTimer = 0;
         stateMachine = _stateMachine;
         dangerousEffect = _danger;
+        tentacle = _tentacle;
     }
 
     public override void Enter()
     {
-
         Debug.Log("Start Attack1");
     }
 
@@ -48,6 +53,7 @@ public class DummyAttack1 : AIState
 
     public override void Exit()
     {
+        dangerousEffect.SetActive(false);
         Debug.Log("End Attack1");
 
     }
@@ -60,8 +66,12 @@ public class DummyAttack1 : AIState
     public override void Update()
     {
         curTimer += Time.deltaTime;
+        BlinkEffect();
         if(curTimer > changeTimer)
         {
+            // 공격 기능.
+            
+            
             if (children.Count > 0)
                 stateMachine.ChangeState(children[current]);
             else if (parent != null)
@@ -72,6 +82,31 @@ public class DummyAttack1 : AIState
                 Debug.Log("연결된 State가 없음.");
 
             curTimer = 0;
+        }
+    }
+
+    void hideTentacle()
+    {
+        
+    }
+
+    // Effect가 점멸하는 기능.
+    void BlinkEffect()
+    {
+        curShowTimer += Time.deltaTime;
+        if(curShowTimer >= showTimer)
+        {
+            if (on)
+            {
+                dangerousEffect.SetActive(false);
+                on = false;
+            }
+            else
+            {
+                dangerousEffect.SetActive(true);
+                on = true;
+            }
+            curShowTimer = 0;
         }
     }
 }
