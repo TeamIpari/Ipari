2821ecf287f02676e7679027598242a45c82aa12
KangMyeongH <71129440+KangMyeongH@users.noreply.github.com>
2023-06-05 23:00:22 +0900

    UIManager 추가

---
+++ WAGTAIL/Assets/01_Scripts/04_Manager/UIManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public enum CanvasType
+{
+    MainMenu,
+    GameUI,
+    EndScreen
+}
+
+public class UIManager : Singleton<UIManager>
+{
+    private List<UIController> _uiControllerList;
+    private UIController _lastActiveUI;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _uiControllerList = GetComponentsInChildren<UIController>().ToList();
+        _uiControllerList.ForEach(x => x.gameObject.SetActive(false));
+        SwitchCanvas(CanvasType.MainMenu);
+    }
+
+    public void SwitchCanvas(CanvasType type) 
+    {
+        if (_lastActiveUI != null)
+        {
+            _lastActiveUI.gameObject.SetActive(false);
+        }
+
+        UIController desiredCanvas = _uiControllerList.Find(x => x.canvasType == type);
+        
+        if (desiredCanvas != null)
+        {
+            desiredCanvas.gameObject.SetActive(true);
+            _lastActiveUI = desiredCanvas;
+        }
+        else {Debug.LogWarning("The desired canvas was not found!");}
+    }
+}
