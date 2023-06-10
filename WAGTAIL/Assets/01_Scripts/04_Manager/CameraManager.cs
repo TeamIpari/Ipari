47e2ff5ef91e91f97953765f8e4e4139ddae556d
KangMyeongH <3044965@naver.com>
2023-06-10 17:20:25 +0900

    죽음 기능 구현 완료

--- WAGTAIL/Assets/01_Scripts/04_Manager/CameraManager.cs
+++ WAGTAIL/Assets/01_Scripts/04_Manager/CameraManager.cs
@@ -19,7 +19,9 @@ public class CameraManager : Singleton<CameraManager>
 {
     private List<CameraController> _cameraControllerList;
 
-    private CameraController _lastActiveCamera;
+    private CameraController _currentCamera;
+
+    private CameraController _prevCamera;
 
     protected override void Awake()
     {
@@ -41,86 +43,38 @@ public class CameraManager : Singleton<CameraManager>
         if (desiredCamera != null)
         {
             desiredCamera.VirtualCamera.MoveToTopOfPrioritySubqueue();
-            _lastActiveCamera = desiredCamera;
+            _prevCamera = _currentCamera;
+            _currentCamera = desiredCamera;
         }
 
         else { Debug.LogWarning("The desired camera was not found!, SwitchCamera() was failed!"); }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    // Debug용 변수들
-    [SerializeField] public bool isChange;
-    [SerializeField] public bool isBack;
-
-    // CM vcam을 linkedlist로 관리
-    [SerializeField] private GameObject cameraGroup;
-    public LinkedList<CinemachineVirtualCamera> list;
-    private LinkedList<CinemachineVirtualCamera> _currentList;
-    
-    private Transform _player;
-    
-    // Start is called before the first frame update
-    private void Awake()
+    public void SwitchPrevCamera()
     {
-        // Cinemachine Camera들을 LinkedList에 넣음
-        var cameraList = cameraGroup.GetComponentsInChildren<CinemachineVirtualCamera>();
-        list = new LinkedList<CinemachineVirtualCamera>(cameraList);
-        _currentList = new LinkedList<CinemachineVirtualCamera>();
-        
-        // Follow, LookAt에 Player.Transform를 넣어야함
-        _player = Player.Instance.GetComponent<Transform>();
-        
-        isChange = false;
-        isBack = false;
-    }
+        var cam = _currentCamera;
 
-    private void Update()
-    {
-        if (isChange)
+        if(_prevCamera != null )
         {
-            ChangeCam();
+            _prevCamera.VirtualCamera.MoveToTopOfPrioritySubqueue();
+            _currentCamera = _prevCamera;
+            _prevCamera = cam;
         }
-        
-        else if (isBack)
+
+        else
         {
-            BackCam();
+            SwitchCamera(CameraType.Main);
+            Debug.LogWarning("PrevCamera was not found! Switch MainCam");
         }
     }
 
-    public void ChangeCam()
+    public CameraController GetCurrentCamera()
     {
-        _currentList.AddFirst(list.First.Value);
-        list.RemoveFirst();
-        list.First.Value.m_Priority = _currentList.First.Value.Priority + 1;
-        list.First.Value.Follow = _player;
-        list.First.Value.LookAt = _player;
-        isChange = false;
+        return _currentCamera;
     }
 
-    public void BackCam()
+    public CameraController GetPrevCamera()
     {
-        _currentList.First.Value.m_Priority = list.First.Value.Priority + 1;
-        list.AddFirst(_currentList.First.Value);
-        _currentList.RemoveFirst();
-        isBack = false;
-    }*/
+        return _prevCamera;
+    }
 }
