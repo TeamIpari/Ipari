6c1cb1ee074eecb642ffc9bce12a2bd5e2896b4b
hoper7748 <hoper7748@gmail.com>
2023-08-14 22:41:19 +0900

    Add Nepenthes Anim
    - 네펜데스 공격 애니메이션 및 공격 딜레이 추가
    - 공격 범위를 벗어나면 재 탐색.

--- WAGTAIL/Assets/01_Scripts/01_NPC/AI Agent/State/Mini_Nepenthes/NepenthesIdleState.cs
+++ WAGTAIL/Assets/01_Scripts/01_NPC/AI Agent/State/Mini_Nepenthes/NepenthesIdleState.cs
@@ -6,16 +6,9 @@ using UnityEngine.InputSystem.XR.Haptics;
 
 public class NepenthesIdleState : AIIdleState
 {
-    int DetectionAngle;
-    float RadianRange;
     bool isSearch ;
-    Vector3 DefualtRot;
     public NepenthesIdleState(AIStateMachine stateMachine, int angle) : base(stateMachine)
     {
-        DetectionAngle = angle;
-        RadianRange = Mathf.Cos((DetectionAngle / 2) * Mathf.Deg2Rad);
-        DefualtRot = stateMachine.character.RotatePoint.rotation.eulerAngles;
-        
     }
 
     public override void Enter()
@@ -38,7 +31,7 @@ public class NepenthesIdleState : AIIdleState
     private bool Search()
     {
         // 원 범위 내에 플레이어가 들어오면 바라보고 일정 주기가 지나면 Attack
-        Collider[] cols = Physics.OverlapSphere(stateMachine.Transform.position, 5.0f);
+        Collider[] cols = Physics.OverlapSphere(stateMachine.Transform.position, stateMachine.character.AttackRange);
 
 
         // Layer가 Player인 캐릭터를 서치 
@@ -49,7 +42,7 @@ public class NepenthesIdleState : AIIdleState
                 float targetRadian = Vector3.Dot(stateMachine.Transform.forward, (cols[i].transform.position - stateMachine.Transform.position).normalized);
 
                 
-                if (/*targetRadian > RadianRange &&*/ cols[i].CompareTag("Player"))
+                if ( cols[i].CompareTag("Player"))
                 {
                     stateMachine.Target = cols[i].gameObject;
                     isSearch = true;
@@ -66,10 +59,11 @@ public class NepenthesIdleState : AIIdleState
         base.Update();
         if (isSearch)
         {
-            // 행렬 변환
-            //stateMachine.character.RotatePoint.transform.LookAt(stateMachine.Target.transform);
-            //Vector3 temp = stateMachine.character.RotatePoint.rotation.eulerAngles;
-            //stateMachine.character.RotatePoint.rotation = Quaternion.Euler(temp.x, temp.y - 90f, temp.z);
+            if (Vector3.Distance(stateMachine.Transform.position, stateMachine.Target.transform.position) > stateMachine.character.AttackRange)
+            {
+                isSearch = false;
+                return;
+            }
 
             Vector3 dir = stateMachine.character.RotatePoint.position - stateMachine.Target.transform.position;
             dir.y = stateMachine.character.RotatePoint.position.y;
@@ -81,14 +75,11 @@ public class NepenthesIdleState : AIIdleState
 
             stateMachine.character.RotatePoint.rotation = Quaternion.Euler(temp.x, temp.y - 180f, temp2.z);
 
-            //stateMachine.ChangeState(stateMachine.character.isAttack() ? stateMachine.character.AiAttack : stateMachine.CurrentState);
+            stateMachine.ChangeState(stateMachine.character.isAttack() ? stateMachine.character.AiAttack : stateMachine.CurrentState);
         }
         else
         {
             Search();
-            //stateMachine.character.RotatePoint.rotation = stateMachine.character.RotatePoint.rotation == Quaternion.Euler(DefualtRot) ? stateMachine.character.RotatePoint.rotation : Quaternion.Euler(DefualtRot);
-            //stateMachine.character.RotatePoint.rotation = Quaternion.Euler(0f, 0f, 0f);
-            //stateMachine.character.RotatePoint.transform.rotation;
         }
 
     }
