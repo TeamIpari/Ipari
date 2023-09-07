d928d85df73f0d0e0ea7d73a839f8bbc34c07f05
JHSeong <50869470+hoper7748@users.noreply.github.com>
2023-09-07 16:41:39 +0900

    Moving Platform

--- WAGTAIL/Assets/01_Scripts/02_Object/MovePlatform/MovingPlatformBehavior.cs
+++ WAGTAIL/Assets/01_Scripts/02_Object/MovePlatform/MovingPlatformBehavior.cs
@@ -1,7 +1,10 @@
+using FMOD;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 /**************************************************
  *  이동하는 플랫폼에 대한 움직임이 정의된 컴포넌트입니다.
@@ -12,50 +15,103 @@ public sealed class MovingPlatformBehavior : PlatformBehaviorBase
     // 현재 위치에서 지정된 방향으로 이동하는 기능을 구현할 것입니다. 네.
     // 상태는 4가지 전조증상(Enter), 내려감(Down), 올라옴(Up), 대기(None),
 
-    private enum LandedType
+    private enum MovingType
     {
-        None,
-        Enter,
-        Down,
-        Up,
+        None = 1,
+        Enter = 2,
+        Down = 3,
+        Up = 4,
+        End = 5,
     }
 
     //========================================
     //////           Property            /////
     //========================================
     [SerializeField] public float       Speed           = 0;
-    [SerializeField] public float       ShakeDistance    = 0;
+    [SerializeField] public float       ShakeDistance   = 0;
     [SerializeField] public float       shakeDelay      = 0;
+    [SerializeField] public float       QuakeRate       = 0f;
     [SerializeField] public Vector3     TargetPosition  = Vector3.zero;
 
 
     //=======================================
     //////      Private Fields          /////
     //=======================================
-    private float                       curTime         = 0f;
+    private float                       _curTime        = 0f;
+    private float                       _curWaitTime    = 0f;
+    private bool                        _isWait         = false;
 
     private Vector3                     _TargetPos      = Vector3.zero;
     private Vector3                     _defaultPos     = Vector3.zero;
-    private LandedType                  _landedType     = LandedType.None;
+    private MovingType                  _movingType     = MovingType.None;
     private Quaternion                  _defaultQuat    = Quaternion.identity;
     private Transform                   _platformTr;
 
-
+    //=======================================
+    /////       Core Method             /////
+    //=======================================
+    
     private void EarthQuake(PlatformObject affectedPlatform)
     {
         /************************************************
          * 전조 현상 LandedType.Enter 일 때 Tile을 흔들어주는 기능을 함.
-         * 왜 이전엔 가능했는가? game object 안에 들어있었기 때문에 0, 0, 0 위치에서 변동이 일어났으므로 가능했음. 때문에 좌표값이 완전히 다른 현 코드에서는 다른 로직을 써야함.
          * **/
-        curTime += Time.deltaTime;
-        if (curTime > shakeDelay)
+        _curTime += Time.deltaTime;
+        if (_curTime > shakeDelay)
         {
             Vector3 offset = _platformTr.position + (UnityEngine.Random.insideUnitSphere * ShakeDistance);
             _TargetPos = new Vector3(offset.x, _platformTr.position.y, offset.z);
-            affectedPlatform.UpdatePosition +=  _TargetPos.normalized;
-            curTime = 0;
+            affectedPlatform.OffsetPosition +=  _TargetPos.normalized * ShakeDistance;
+            _curTime = 0;
         }
     }
+    private void MoveToWordsPlatform(PlatformObject affectedPlatform)
+    {
+        /************************************************
+         * 전조현상이 끝나면 첫 위치에서 최종적으로 이동할 위치까지 연산함.
+         * **/
+        UnityEngine.Debug.Log($"Move To Words");
+        Vector3 direction = affectedPlatform.UpdatePosition - (_defaultPos + TargetPosition);       // 방향 벡터 결정
+        float distance = Vector3.Distance(affectedPlatform.UpdatePosition, (_defaultPos + TargetPosition));
+        if(distance > 0.1f)
+        {
+            affectedPlatform.UpdatePosition -= direction.normalized * (Time.deltaTime * Speed);
+        }
+        else
+        {
+            StartPlatformStateChange(1f);
+        }
+    }
+
+    private void MoveToOriginPlatform(PlatformObject affectedPlatform)
+    {
+        UnityEngine.Debug.Log($"Move To Origin");
+        Vector3 direction = _defaultPos - affectedPlatform.UpdatePosition;       // 방향 벡터 결정
+        float distance = Vector3.Distance(affectedPlatform.UpdatePosition, _defaultPos );
+        if (distance > 0.1f)
+        {
+            affectedPlatform.UpdatePosition += direction.normalized * (Time.deltaTime * Speed);
+        }
+        else
+        {
+            StartPlatformStateChange(0f);
+        }
+    }
+
+    private void StartPlatformStateChange(float rate)
+    {
+        /***************************************
+         * 다음 State로 변경되는데 걸리는 시간.
+         * **/
+        _curWaitTime += Time.deltaTime;
+        if(_curWaitTime > rate)
+        {
+            _movingType = MovingType.Up != _movingType ? _movingType  + 1 : MovingType.None;
+            _curWaitTime = 0;
+        }
+        
+    }
+
 
     //=======================================
     //////      Override Methods          ////
@@ -70,25 +126,28 @@ public sealed class MovingPlatformBehavior : PlatformBehaviorBase
 
     public override void PhysicsUpdate(PlatformObject affectedPlatform)
     {
-        switch (_landedType)
+        switch (_movingType)
         {
-            case LandedType.None:
+            case MovingType.None:
                 // 아무런 기능을 하지 않음.
                 break;
-            case LandedType.Enter:
+            case MovingType.Enter:
                 // 흔들림 현상
                 EarthQuake(affectedPlatform);
+                StartPlatformStateChange(QuakeRate);
                 break;
-            case LandedType.Down:
-
+            case MovingType.Down:
+                MoveToWordsPlatform(affectedPlatform);
                 break;
-            case LandedType.Up:
+            case MovingType.Up:
+                MoveToOriginPlatform(affectedPlatform);
 
                 break;
             default:
                 break;
         }
-        Debug.Log("BB");
+        // waitTimer;       다음 State로 변경하기 위해 기다리는 시간.
+
     }
 
     public override void OnObjectPlatformEnter(PlatformObject affectedPlatform, GameObject standingTarget, Rigidbody standingBody, Vector3 standingPoint, Vector3 standingNormal)
@@ -97,9 +156,8 @@ public sealed class MovingPlatformBehavior : PlatformBehaviorBase
          *  플랫폼을 밟으면 밟은 시점에 전조 증상이 발생하게 enumState를 변경. 이후 흔들리는 기능을 넣는다.
          *  **/
 
-        _landedType = LandedType.Enter;         // 전조 증상 발생
+        _movingType = MovingType.Enter;         // 전조 증상 발생
 
-        Debug.Log("AA");
     }
 
 }
