0bf071b3765aa235e341174ae1aeb3d0ca4d0a33
KangMyeongH <71129440+KangMyeongH@users.noreply.github.com>
2023-09-09 10:39:51 +0900

    폭탄열매 구현 완료 및 잡고 던지기 중간 개선
    1. 플레이어 피격 시 LagDoll로 변환 시켜서 날리는 기능만 추가하면 됨
    2. 잡고 던지기 중간의 개선

--- WAGTAIL/Assets/01_Scripts/02_Object/InteractionSystem/BombObject.cs
+++ WAGTAIL/Assets/01_Scripts/02_Object/InteractionSystem/BombObject.cs
@@ -7,51 +7,102 @@ using UnityEngine.Serialization;
 public class BombObject : MonoBehaviour
 {
     [SerializeField] private LayerMask layerMask;
-    [SerializeField] private float force;
     [SerializeField] private float explosionRange;
+    [SerializeField] private float explosionTime;
+    [SerializeField] private GameObject explosionVFX;
 
-    private const float PushTime = 0.075f;
-    private readonly Collider[] _colliders = new Collider[1];
-    
+    private readonly Collider[] _colliders = new Collider[10];
+    private Color _baseColor;
+    private Renderer _color;
     private float _currentTime;
     private CharacterController _cc;
     private Vector3 _vDir;
-    private bool _isStart;
     
+    // Test property // 
+    private float force = 13f;
+    private bool _isStart;
+    private bool _isExplosionVFXNotNull;
+    private const float PushTime = 0.075f;
 
     // Start is called before the first frame update
     private void Start()
     {
+        _isExplosionVFXNotNull = explosionVFX != null;
+        _color = GetComponent<Renderer>();
+        _baseColor = _color.material.color;
         _currentTime = 0;
         _isStart = false;
+        //StartCoroutine(StartTimeBomb(explosionTime));
     }
-
-    // Update is called once per frame
-    private void Update()
+    
+    private IEnumerator StartTimeBomb(float time)
     {
-        if(Input.GetKeyDown(KeyCode.G))
+        float currentTime = time;
+        float totalTime = 0f;
+        StartCoroutine(Bomb());
+        while (totalTime < time)
         {
-            StartExplosion();
+            _color.material.color = Color.red;
+            yield return new WaitForSeconds(0.2f);
+            _color.material.color = _baseColor;
+            yield return new WaitForSeconds(currentTime / time - 0.2f);
+            totalTime += currentTime / time;
+            currentTime -= currentTime / time;
         }
     }
-    
-    
-    private void FixedUpdate()
+
+    private IEnumerator Bomb()
     {
-        if (_isStart) MovePlayer();
+        yield return new WaitForSeconds(5f);
+        StopCoroutine(nameof(StartTimeBomb));
+        Explosion();
     }
     
-    public void StartExplosion()
+    public void Explosion()
     {
-        var numFound = Physics.OverlapSphereNonAlloc(transform.position, explosionRange, _colliders, layerMask);
-        if (numFound == 0) return;
+        var tf = transform;
+        var size = Physics.OverlapSphereNonAlloc(tf.position, explosionRange, _colliders);
+        Debug.Log(size);
+        if (size != 0)
+        {
+            for (var i = 0; i < size; i++)
+            {
+                if (_colliders[i].CompareTag("Player"))
+                    _colliders[i].GetComponent<Player>().isDead = true;
+                
+                else if (_colliders[i].CompareTag("Platform"))
+                    _colliders[i].GetComponent<IEnviroment>().ExecutionFunction(0.0f);
+            }
+        }
         
-        _cc = _colliders[0].GetComponent<CharacterController>();
+        if (_isExplosionVFXNotNull )         
+        {
+            var exploVFX = Instantiate(explosionVFX, tf.position, tf.rotation);
+            Destroy(exploVFX, 3);
+        }
+        Destroy(gameObject);
+
+
+        /*_cc = _colliders[0].GetComponent<CharacterController>();
         CalcDirectionVector(_colliders[0].transform.position, transform.position);
         _currentTime = 0f;
-        _isStart = true;
+        _isStart = true;*/
     }
     
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRange);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            StartCoroutine(StartTimeBomb(explosionTime));
+        }
+    }
+
     private void CalcDirectionVector(Vector3 targetPos, Vector3 currentPos)
     {
         _vDir = (targetPos - currentPos).normalized;
@@ -70,12 +121,5 @@ public class BombObject : MonoBehaviour
         {
             _isStart = false;
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, explosionRange);
-    }
-    
+    }    
 }
