3fd4aaef4f8c2fee3463fd398554b2726e38320a
JHSeong <50869470+hoper7748@users.noreply.github.com>
2023-09-10 23:26:36 +0900

    Commit
    1. Larva를 생성할 때 Size를 조절하는 기능 추가.
    2. 위의 Larva를 생성하는 Spawner Prefab화

--- WAGTAIL/Assets/01_Scripts/01_NPC/AITest/MovePoint.cs
+++ WAGTAIL/Assets/01_Scripts/01_NPC/AITest/MovePoint.cs
@@ -10,27 +10,24 @@ public class MovePoint : MonoBehaviour
     [Header("Create Rot Around Objects")]
     [Range(3, 100)]
     public int Polygon = 3;
-    public float CircleSize = 1.0f;
     public Transform Center;
     public Vector3[] SpawnVertices;
     public List<GameObject> Objs;
     public List<GameObject> Larvas;
     public Vector3 Offset = new Vector3(0, 0, 0);
     public float caculate;  // defualt 6.25f -> 4.75f
+    public float initPrefabSize = 1.0f;
 
     public int CreatePoint;
     public float MoveSpeed = 0; 
 
     public GameObject[] LarvaPrefabs;
-    public GameObject HeadObj;
-    public GameObject BodyObj;
     public bool Reverse = false;
-
-    //public int tails;
-    int count = 1;
+    
     public float DamagedAnimTimer = 1.5f;
-
-    float angle = 0;
+    //public int tails;
+    [HideInInspector] public float CircleSize = 1.0f;
+    private int count = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -38,7 +35,7 @@ public class MovePoint : MonoBehaviour
         // 중심 설정
         if (Center == null)
             Center = this.transform;
-        CircleSize = Polygon / caculate;
+        CircleSize = Polygon / (caculate / initPrefabSize);
         CreatePoint = Polygon / CreatePoint;
         if (MoveSpeed == 0)
             MoveSpeed = 50f;
@@ -83,7 +80,7 @@ public class MovePoint : MonoBehaviour
                     GameObject obj2 = Instantiate(LarvaPrefabs[LarvaCur--]);
                     obj2.transform.rotation = Quaternion.Euler(0f , 0f ,0f);
                     obj2.AddComponent<AutoMoveLarva>().SetUp(this, ver2 % 2 == 0 ? false: true);
-
+                    obj2.transform.localScale = new Vector3(initPrefabSize, initPrefabSize, initPrefabSize);
                     obj2.transform.position = Objs[ver2 - 1].transform.position;
                     Larvas.Add(obj2);
                 }
