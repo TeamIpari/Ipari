270f487ee4a8b7d17f49c35af2aa40cffe85b70c
KangMyeongH <71129440+KangMyeongH@users.noreply.github.com>
2023-10-28 01:50:56 +0900

    Save/Load를 위한 DataManager 추가

---
+++ WAGTAIL/Assets/01_Scripts/04_Manager/DataManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataManager : MonoBehaviour
+{
+    private static GameObject _container;
+    
+    private static DataManager _instance;
+
+    public static DataManager Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _container = new GameObject();
+                _container.name = "DataManager";
+                _instance = _container.AddComponent(typeof(DataManager)) as DataManager;
+                DontDestroyOnLoad(_container);
+            }
+            return _instance;
+        }
+    }
+    
+    // 게임 데이터 파일이름 설정
+    private string _gameDataFileName = "GameData.json";
+    
+    // 게임 데이터 저장용 클래스
+    public GameData data = new GameData();
+    
+    // 불러오기
+    public void LoadGameData()
+    {
+        string filePath = Application.persistentDataPath + "/" + _gameDataFileName;
+        
+        // 저장된 게임이 있다면
+        if (File.Exists(filePath))
+        {
+            // 저장된 파일을 읽어옴
+            string fromJsonData = File.ReadAllText(filePath);
+            data = JsonUtility.FromJson<GameData>(fromJsonData);
+#if UNITY_EDITOR
+            Debug.Log("Load GameData Finish");
+#endif
+        }
+    }
+
+    public void SaveGameDate()
+    {
+        // Json으로 변환
+        string toJsonData = JsonUtility.ToJson(data, true);
+        string filePath = Application.persistentDataPath + "/" + _gameDataFileName;
+        
+        // 파일 저장
+        File.WriteAllText(filePath, toJsonData);
+        
+#if UNITY_EDITOR
+        Debug.Log("Save GameData Finish");
+#endif
+    }
+}
---
+++ WAGTAIL/Assets/01_Scripts/04_Manager/GameData.cs
@@ -0,0 +1,15 @@
+using System;
+
+[Serializable]
+
+public class GameData
+{
+    // ภ๚ภๅวา Property
+    public float masterVolume;
+    public float bgmVolume;
+    public float sfxVolume;
+    
+    public ChapterType currentChapterType;
+    public bool[][] cocosi;
+
+}
